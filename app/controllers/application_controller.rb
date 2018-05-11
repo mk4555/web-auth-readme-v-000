e05de9c8d76c112f392b07cafdc9bc3ddd63a48e
github username <github email address>
2018-05-11 04:47:12 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :authenticate_user
+  def logged_in?
+    !!session[:token]
+  end
 
-end
+  def authenticate_user
+    client_id = 'MGF3UJT4M45HI0VBYAPRIEPC2NOULAQNUIH5IULF3KTSY0NC'
+    secret = 'AM03LYNWMBRFCEPSNOBZAUJ5MNYJ2MYRIDU5LBPGLIJFIFIA'
+    redirect_uri = CGI.escape("http://localhost:3000/auth")
+    foursquare_url = "https://foursquare.com/oauth2/authenticate?client_id=#{client_id}&response_type=code&redirect_uri=#{redirect_uri}"
+    redirect_to foursquare_url unless logged_in?
+  end
+  end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,17 @@
 class SessionsController < ApplicationController
+  skip_before_action :authenticate_user, only :create
 
+  def create
+  resp = Faraday.get("https://foursquare.com/oauth2/access_token") do |req|
+    req.params['client_id'] = 'MGF3UJT4M45HI0VBYAPRIEPC2NOULAQNUIH5IULF3KTSY0NC'
+    req.params['client_secret'] = 'AM03LYNWMBRFCEPSNOBZAUJ5MNYJ2MYRIDU5LBPGLIJFIFIA'
+    req.params['grant_type'] = 'authorization_code'
+    req.params['redirect_uri'] = "http://localhost:3000/auth"
+    req.params['code'] = params[:code]
+  end
+
+  body = JSON.parse(resp.body)
+  session[:token] = body["access_token"]
+  redirect_to root_path
+end
 end
